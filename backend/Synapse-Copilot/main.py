947f3dad75c3a00b4ffac04895b82f672c2d8837
Sheape <86521166+Sheape@users.noreply.github.com>
2024-06-30 00:27:55 +0800

    fix: error handling with post request

--- backend/Synapse-Copilot/main.py
+++ backend/Synapse-Copilot/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 from typing import List, Dict
 import json
@@ -19,18 +20,26 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+class MessageQuery(BaseModel):
+    model: str
+    message: str
 
 @app.get("/")
 def home():
     return { "status": "OK", "API version": __version__ }
 
 @app.post("/chat/completions")
-def chat_completion(model: str, message: str):
-    if model == "synapse-copilot":
-        main(message)
-        data = None
-        with open("output.json", "r") as json_file:
-            data = json.load(json_file)
-        return { "data": data }
+def chat_completion(msg_query: MessageQuery):
+    if msg_query.model == "synapse-copilot":
+        try:
+            main(msg_query.message)
+            with open("output.json", "r") as json_file:
+                data = json.load(json_file)
+            return { "data": data }
+        except Exception as e:
+            # Log the exception for debugging purposes
+            print(str(e))
+            # Return an error response to the client
+            raise HTTPException(status_code=500, detail=str(e))
     else:
-        raise HTTPException(status_code=404, detail=f"Model {model} not found.")
+        raise HTTPException(status_code=404, detail=f"Model {msg_query.model} not found.")
